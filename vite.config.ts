59f3f563391efc61bbadc41f734b60929160871f
MatheusDev-sys <matheusvinicios88@gmail.com>
2025-07-31 04:16:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/a/' : '/',
+  base: mode === 'production' ? '/kinetic-folio-journey/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
